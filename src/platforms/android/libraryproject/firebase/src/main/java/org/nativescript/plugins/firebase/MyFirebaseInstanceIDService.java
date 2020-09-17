0ac84d009ba3a93befbfdcdf10ed9cd8c8f7727a
Jun Ying <jying@simplec.com>
2020-09-16 22:27:13 -0400

    Updated to conform to current version of firebase.

--- src/platforms/android/libraryproject/firebase/src/main/java/org/nativescript/plugins/firebase/MyFirebaseInstanceIDService.java
+++ src/platforms/android/libraryproject/firebase/src/main/java/org/nativescript/plugins/firebase/MyFirebaseInstanceIDService.java
@@ -1,15 +1,11 @@
 package org.nativescript.plugins.firebase;
 
-import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.iid.FirebaseInstanceIdService;
+import com.google.firebase.messaging.FirebaseMessagingService;
 
-public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+public class MyFirebaseInstanceIDService extends FirebaseMessagingService {
 
   @Override
-  public void onTokenRefresh() {
-    // Get updated InstanceID token (may be null in case the device has no internet)
-    final String refreshedToken = FirebaseInstanceId.getInstance().getToken();
-
+  public void onNewToken(String refreshedToken) {
     // Send to client
     FirebasePlugin.executeOnPushTokenReceivedCallback(refreshedToken);
   }
